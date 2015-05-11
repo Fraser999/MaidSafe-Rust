2474bf9ec9d6139c28dc9789a7be64b58532ff33
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-05-11 09:13:37 +0100

    Update.

--- Initialise.sh
+++ Initialise.sh
@@ -8,9 +8,9 @@ echo "==========================================================================
 for i in `grep path .gitmodules | sed 's/.*= //'` ; do
   echo -e "Configuring $i\n------------`echo "$i" | tr [:print:] -`"
 
-  git -C $i remote add dirvine `git -C $i config --get remote.origin.url | sed 's/github.com:Fraser999/github.com:dirvine/'`
-  git -C $i remote set-url --push dirvine disable_push
-  git -C $i fetch dirvine
+  git -C $i remote add upstream `git -C $i config --get remote.origin.url | sed 's/github.com:Fraser999/github.com:upstream/'`
+  git -C $i remote set-url --push upstream disable_push
+  git -C $i fetch upstream
 
   echo ""
   git -C $i remote -v
