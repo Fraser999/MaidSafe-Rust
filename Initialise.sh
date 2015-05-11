0713b5472f4dd5789894ef0e94329229dcb86175
Fraser Hutchison <fraser.hutchison@maidsafe.net>
2015-05-11 09:24:17 +0100

    Update.

--- Initialise.sh
+++ Initialise.sh
@@ -8,7 +8,7 @@ echo "==========================================================================
 for i in `grep path .gitmodules | sed 's/.*= //'` ; do
   echo -e "Configuring $i\n------------`echo "$i" | tr [:print:] -`"
 
-  git -C $i remote add upstream `git -C $i config --get remote.origin.url | sed 's/github.com:Fraser999/github.com:upstream/'`
+  git -C $i remote add upstream `git -C $i config --get remote.origin.url | sed 's/github.com:Fraser999/github.com:maidsafe/'`
   git -C $i remote set-url --push upstream disable_push
   git -C $i fetch upstream
 
